16898d3decc526dd976493d16d08c8b89418ea28
guicostaarantes <arantes.g@outlook.com>
2021-03-03 22:11:14 -0300

    💄 fixed emojiradio for safari

--- src/styleguide/EmojiRadio/index.tsx
+++ src/styleguide/EmojiRadio/index.tsx
@@ -11,6 +11,10 @@ interface EmojiRadioProps {
   }[];
 }
 
+const MARGIN = 0.2;
+const ICON_WIDTH = 1.5;
+const ZOOM_ICON_WIDTH = 2.5;
+
 const EmojiRadio = ({
   checkedValue,
   name,
@@ -21,7 +25,7 @@ const EmojiRadio = ({
 
   return (
     <>
-      <div>
+      <div className="wrapper">
         {options.map((opt) => (
           <label key={opt.label}>
             <input
@@ -33,16 +37,19 @@ const EmojiRadio = ({
               type="radio"
               value={opt.value}
             />
-            <img src={opt.src} alt={opt.label} title={opt.label} />
+            <div className="image">
+              <img src={opt.src} alt={opt.label} title={opt.label} />
+            </div>
           </label>
         ))}
       </div>
       <style jsx>{`
-        div {
+        .wrapper {
           align-items: center;
           display: flex;
-          height: 2.5rem;
-          width: 11.5rem;
+          height: ${ZOOM_ICON_WIDTH}rem;
+          width: ${2 * ZOOM_ICON_WIDTH +
+          (options.length - 1) * (ICON_WIDTH + 2 * MARGIN)}rem;
         }
         label {
           display: flex;
@@ -50,28 +57,29 @@ const EmojiRadio = ({
         img {
           border-radius: 15%;
           cursor: pointer;
-          margin: 0.2rem;
-          width: 1.5rem;
+          margin: ${MARGIN}rem;
+          width: ${ICON_WIDTH}rem;
           transition: filter 0.3s ease, width 0.3s ease;
         }
         input {
-          margin: 0;
+          appearance: none;
           height: 0;
+          margin: 0;
           width: 0;
         }
-        input:focus:not(:active) + img {
+        input:focus:not(:active) + .image img {
           box-shadow: 0 0 0 0.1rem ${theme.primaryColorHover}77;
         }
-        input:checked + img {
+        input:checked + .image img {
           filter: saturate(200%);
-          width: 2.5rem;
+          width: ${ZOOM_ICON_WIDTH}rem;
         }
-        div:not(:hover) input:not(:checked) + img {
+        div:not(:hover) input:not(:checked) + .image img {
           filter: saturate(0%);
         }
         img:hover {
           filter: saturate(200%);
-          width: 2.5rem;
+          width: ${ZOOM_ICON_WIDTH}rem;
         }
       `}</style>
     </>
