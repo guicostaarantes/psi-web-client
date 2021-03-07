cab78f1e8726bbddb4220f2d576bf10978c79530
guicostaarantes <arantes.g@outlook.com>
2021-03-07 09:57:53 -0300

    ✨ hook usePagePermission

---
+++ modules/auth/hooks/usePagePermission/graphql.ts
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+import { Role } from "@psi/auth/constants/roles";
+
+export interface GetOwnUserResponseData {
+  id: string;
+  role: Role;
+}
+
+export interface GetOwnUserResponse {
+  getOwnUser: GetOwnUserResponseData;
+}
+
+export const GetOwnUser = gql`
+  query GetOwnUser {
+    getOwnUser {
+      id
+      role
+    }
+  }
+`;
+
+export interface GetOwnPatientProfileResponse {
+  getOwnPatientProfile: {
+    fullName: string;
+  };
+}
+
+export const GetOwnPatientProfile = gql`
+  query GetOwnPatientProfile {
+    getOwnPatientProfile {
+      fullName
+    }
+  }
+`;
---
+++ modules/auth/hooks/usePagePermission/spec.tsx
@@ -0,0 +1,225 @@
+import { GraphQLError } from "graphql";
+import { MockedProvider } from "@apollo/client/testing";
+import usePagePermission from "@psi/auth/hooks/usePagePermission";
+import {
+  GetOwnPatientProfile,
+  GetOwnUser,
+} from "@psi/auth/hooks/usePagePermission/graphql";
+import { render, screen, waitFor } from "@testing-library/react";
+
+const mockPushRoute = jest
+  .fn()
+  .mockImplementation(() => new Promise((resolve) => resolve(0)));
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: mockPushRoute,
+  }),
+}));
+
+beforeEach(() => {
+  mockPushRoute.mockClear();
+});
+
+const successMock = [
+  {
+    request: { query: GetOwnUser },
+    result: {
+      data: {
+        getOwnUser: {
+          id: "6c82b9eb-722a-48f5-9418-0fcd3fbbca47",
+          role: "PSYCHOLOGIST",
+        },
+      },
+    },
+  },
+  {
+    request: { query: GetOwnPatientProfile },
+    result: {
+      data: {
+        getPatientProfile: {
+          fullName: "Thomas Edward Patrick Brady, Jr.",
+        },
+      },
+    },
+  },
+];
+
+const notAuthenticatedMock = [
+  {
+    request: { query: GetOwnUser },
+    result: {
+      errors: [new GraphQLError("forbidden")],
+    },
+  },
+  {
+    request: { query: GetOwnPatientProfile },
+    result: {
+      errors: [new GraphQLError("forbidden")],
+    },
+  },
+];
+
+const userWithoutPatientProfileMock = [
+  {
+    request: { query: GetOwnUser },
+    result: {
+      data: {
+        getOwnUser: {
+          id: "6c82b9eb-722a-48f5-9418-0fcd3fbbca47",
+          role: "PATIENT",
+        },
+      },
+    },
+  },
+  {
+    request: { query: GetOwnPatientProfile },
+    result: {
+      errors: [new GraphQLError("resource not found")],
+    },
+  },
+];
+
+const PublicComponent = () => {
+  const { pageStatus } = usePagePermission({});
+
+  return <div>current pageStatus is {pageStatus}</div>;
+};
+
+test("should not retrieve user if page is public", async () => {
+  render(
+    <MockedProvider mocks={successMock}>
+      <PublicComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
+
+test("should not redirect unauthenticated visitor since the page is public", async () => {
+  render(
+    <MockedProvider mocks={notAuthenticatedMock}>
+      <PublicComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
+
+const PrivateComponent = () => {
+  const { pageStatus } = usePagePermission({
+    requiresAuth: true,
+  });
+
+  return <div>current pageStatus is {pageStatus}</div>;
+};
+
+test("should show id if page requires login", async () => {
+  render(
+    <MockedProvider mocks={successMock}>
+      <PrivateComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
+
+test("should redirect to /login if user not found", async () => {
+  render(
+    <MockedProvider mocks={notAuthenticatedMock}>
+      <PrivateComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).toBeCalledWith("/login");
+  });
+});
+
+const NeedsPatientProfileComponent = () => {
+  const { pageStatus } = usePagePermission({
+    requiresAuth: true,
+    requiresPatientProfile: true,
+  });
+
+  return <div>current pageStatus is {pageStatus}</div>;
+};
+
+test("should show id if patient profile is found", async () => {
+  render(
+    <MockedProvider mocks={successMock}>
+      <NeedsPatientProfileComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
+
+test("should redirect to /paciente if patient profile not found", async () => {
+  render(
+    <MockedProvider mocks={userWithoutPatientProfileMock}>
+      <NeedsPatientProfileComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).toBeCalledWith("/paciente");
+  });
+});
+
+const RestrictToPsychologistComponent = () => {
+  const { pageStatus } = usePagePermission({
+    requiresAuth: true,
+    requiresRole: ["PSYCHOLOGIST"],
+  });
+
+  return <div>current pageStatus is {pageStatus}</div>;
+};
+
+test("should get pageStatus === notFound if user is logged in but has forbidden role", async () => {
+  render(
+    <MockedProvider mocks={userWithoutPatientProfileMock}>
+      <RestrictToPsychologistComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is notFound");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
+
+test("should get pageStatus === ready if user is logged in with permitted role", async () => {
+  render(
+    <MockedProvider mocks={successMock}>
+      <RestrictToPsychologistComponent />
+    </MockedProvider>,
+  );
+
+  await waitFor(() => {
+    const text = screen.getByText("current pageStatus is ready");
+    expect(text).toBeInTheDocument();
+    expect(mockPushRoute).not.toBeCalled();
+  });
+});
