3596348dbb191d9fcd043540644c2f439cd22a69
Guilherme Arantes <guilherme.arantes@aula.education>
2021-06-19 10:18:54 -0300

    ✨ handling treatment clash gracefully

--- modules/home/components/PsychologistTreatments/components/NewTreatmentButton.tsx
+++ modules/home/components/PsychologistTreatments/components/NewTreatmentButton.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from "@apollo/client";
 import { useState } from "@hookstate/core";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import {
   CreateTreatment,
@@ -11,8 +11,11 @@ import Button from "@psi/styleguide/components/Button";
 import Input from "@psi/styleguide/components/Input";
 import Modal from "@psi/styleguide/components/Modal";
 import Paragraph from "@psi/styleguide/components/Typography/Paragraph";
+import useToast from "@psi/styleguide/hooks/useToast";
 
 const NewTreatmentButton = () => {
+  const { addToast } = useToast();
+
   const weekday = useRef(null);
   const start = useRef(null);
   const duration = useRef(null);
@@ -20,7 +23,7 @@ const NewTreatmentButton = () => {
 
   const modalOpen = useState(false);
 
-  const [createTreatment, { loading }] = useMutation<
+  const [createTreatment, { loading, error }] = useMutation<
     null,
     CreateTreatmentInput
   >(CreateTreatment, {
@@ -32,18 +35,41 @@ const NewTreatmentButton = () => {
   const handleModalClose = () => modalOpen.set(false);
 
   const handleCreateConfirmClick = async () => {
-    await createTreatment({
-      variables: {
-        weeklyStart:
-          86400 * Number(weekday.current.value) +
-          Number(start.current.value) +
-          60 * new Date().getTimezoneOffset(),
-        duration: 60 * Number(duration.current.value),
-        price: Number(price.current.value),
-      },
-    });
-    modalOpen.set(false);
+    try {
+      await createTreatment({
+        variables: {
+          weeklyStart:
+            86400 * Number(weekday.current.value) +
+            Number(start.current.value) +
+            60 * new Date().getTimezoneOffset(),
+          duration: 60 * Number(duration.current.value),
+          price: Number(price.current.value),
+        },
+      });
+    } catch (err) {
+      // empty
+    }
   };
+
+  useEffect(() => {
+    if (error) {
+      if (error.message === "there is another treatment in the same period") {
+        addToast({
+          header: "Horários conflitantes",
+          message:
+            "O horário e a duração do novo tratamento está colidindo com outro tratamento. Tente novamente.",
+        });
+      } else {
+        addToast({
+          header: "Erro no servidor",
+          message:
+            "O servidor do PSI retornou um erro. Tente novamente mais tarde.",
+        });
+      }
+    } else if (!loading) {
+      modalOpen.set(false);
+    }
+  }, [loading, error]);
 
   return (
     <>
