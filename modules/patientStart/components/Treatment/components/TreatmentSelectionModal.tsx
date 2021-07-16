ce2914fab069bfe82b131cf72eb1a8c0919f974b
Guilherme Arantes <arantes.g@outlook.com>
2021-07-15 21:12:39 -0300

    ✨ treatment selection modal

---
+++ modules/patientStart/components/Treatment/components/TreatmentSelectionModal.tsx
@@ -0,0 +1,141 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useState } from "@hookstate/core";
+import { useEffect } from "react";
+
+import { MyPatientAppointments } from "@psi/patientStart/components/Appointment/graphql";
+import { MyPatientTreatments } from "@psi/patientStart/components/Treatment/graphql";
+import {
+  AssignTreatment,
+  AssignTreatmentInput,
+  MyPatientTopAffinities,
+  MyPatientTopAffinitiesResponse,
+} from "@psi/patientStart/components/Treatment/graphql";
+import formatWeeklyHour from "@psi/shared/utils/formatWeeklyHour";
+import Button from "@psi/styleguide/components/Button";
+import Card from "@psi/styleguide/components/Card";
+import Modal from "@psi/styleguide/components/Modal";
+import Radio from "@psi/styleguide/components/Radio";
+import Paragraph from "@psi/styleguide/components/Typography/Paragraph";
+import useToast from "@psi/styleguide/hooks/useToast";
+
+interface TreatmentSelectionModalProps {
+  onClose: () => void;
+  open: boolean;
+}
+
+const TreatmentSelectionModal = ({
+  onClose,
+  open,
+}: TreatmentSelectionModalProps) => {
+  const { addToast } = useToast();
+
+  const { data } = useQuery<MyPatientTopAffinitiesResponse>(
+    MyPatientTopAffinities,
+    {
+      fetchPolicy: "no-cache",
+    },
+  );
+
+  const [assignTreatment, { loading, error }] = useMutation<
+    null,
+    AssignTreatmentInput
+  >(AssignTreatment, {
+    awaitRefetchQueries: true,
+    refetchQueries: [
+      { query: MyPatientAppointments },
+      { query: MyPatientTreatments },
+    ],
+  });
+
+  useEffect(() => {
+    if (error) {
+      addToast({
+        header: "Erro ao iniciar tratamento",
+        message:
+          "Isso pode ter acontecido pois o tratamento escolhido foi selecionado recentemente por outro paciente. Escolha outro horário. Se o erro persistir, tente novamente mais tarde.",
+      });
+    }
+  }, [error]);
+
+  const selectedTreatment = useState("");
+
+  const handleAssignClick = async () => {
+    try {
+      await assignTreatment({ variables: { id: selectedTreatment.value } });
+    } catch (err) {
+      // empty
+    }
+  };
+
+  const topAffinities =
+    data?.myPatientTopAffinities?.filter((aff) => aff.psychologist) || [];
+
+  return (
+    <Modal open={open} onClose={onClose} title="Escolher psicólogo">
+      {loading ? (
+        <Paragraph>
+          Aguarde enquanto procuramos os psicólogos que estão disponíveis e
+          possuem a maior sinergia com as suas preferências.
+        </Paragraph>
+      ) : topAffinities.length ? (
+        <Paragraph>
+          Escolha um dos psicólogos e horários abaixo para iniciar o tratamento.
+        </Paragraph>
+      ) : (
+        <Paragraph>
+          Não conseguimos encontrar nenhum psicólogo que esteja disponível no
+          momento. Estamos sempre tentando aumentar nossa rede, pedimos que
+          tente novamente amanhã.
+        </Paragraph>
+      )}
+      {topAffinities.map((aff) => (
+        <Card floating key={aff.psychologist.id}>
+          <div>{aff.psychologist.fullName}</div>
+          <div className="radio-group">
+            {aff.psychologist.pendingTreatments.map((tr) => (
+              <div key={tr.id}>
+                <Radio
+                  name={tr.id}
+                  value={tr.id}
+                  label={`Sessões ${formatWeeklyHour(tr.weeklyStart)}`}
+                  checked={tr.id === selectedTreatment.value}
+                  onChange={() => selectedTreatment.set(tr.id)}
+                />
+              </div>
+            ))}
+          </div>
+        </Card>
+      ))}
+      <div className="buttons">
+        <Button
+          color="primary"
+          disabled={!selectedTreatment.value}
+          loading={loading}
+          onClick={handleAssignClick}
+        >
+          Iniciar tratamento
+        </Button>
+        <Button color="secondary" onClick={onClose}>
+          Voltar
+        </Button>
+      </div>
+      <style jsx>{`
+        .buttons {
+          align-items: center;
+          display: flex;
+          gap: 0.5rem;
+          justify-content: flex-end;
+        }
+        .radio-group {
+          display: flex;
+          flex-direction: column;
+          gap: 0.5rem;
+          margin-bottom: 0.5rem;
+          margin-top: 0.5rem;
+        }
+      `}</style>
+    </Modal>
+  );
+};
+
+export default TreatmentSelectionModal;
