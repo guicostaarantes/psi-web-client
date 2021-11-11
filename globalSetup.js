9f23ca8d0ff0027c382db6fe6c51e11999b8d0c3
Guilherme Arantes <arantes.g@outlook.com>
2021-11-11 15:35:15 -0300

    🐛 suppress error not find certificate

--- globalSetup.js
+++ globalSetup.js
@@ -2,5 +2,5 @@ module.exports = async () => {
   // this makes test results independent of client node timezone
   process.env.TZ = "America/Lima";
 
-  process.env.NEXT_PUBLIC_PSI_STATIC_URI = "http://localhost/static";
+  process.env.NEXT_PUBLIC_PSI_STATIC_URI = "https://www.google.com/static";
 };
