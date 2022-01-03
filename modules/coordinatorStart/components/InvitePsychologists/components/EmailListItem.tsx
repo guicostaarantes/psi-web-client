869fb04444fc51ccfc383dce624835f577d66a00
Guilherme Arantes <arantes.g@outlook.com>
2022-01-03 08:43:41 -0300

    ✨ component to invite psys via email

---
+++ modules/coordinatorStart/components/InvitePsychologists/components/EmailListItem.tsx
@@ -0,0 +1,72 @@
+import { none, State, useState } from "@hookstate/core";
+import React, { forwardRef, useImperativeHandle } from "react";
+import { MdClose, MdDone, MdErrorOutline } from "react-icons/md";
+
+import { useInvitePsychologistUserMutation } from "@psi/shared/graphql";
+import CircleSpinner from "@psi/styleguide/components/Spinner";
+
+interface EmailListItemProps {
+  emailState: State<string>;
+}
+
+export interface EmailListItemRef {
+  sendInvite: () => void;
+}
+
+const EmailListItem = ({ emailState }: EmailListItemProps, ref) => {
+  const email = useState(emailState);
+
+  const [
+    invitePsychologist,
+    { loading, error, data },
+  ] = useInvitePsychologistUserMutation();
+
+  useImperativeHandle(ref, () => ({
+    sendInvite,
+  }));
+
+  const sendInvite = () => {
+    if (!loading && !data) {
+      invitePsychologist({ variables: { email: email.value } });
+    }
+  };
+
+  const handleRemove = () => email.set(none);
+
+  return (
+    <li>
+      {loading ? (
+        <button onClick={() => null}>
+          <CircleSpinner size="1.5rem" />
+        </button>
+      ) : error ? (
+        <button onClick={sendInvite}>
+          <MdErrorOutline size="1.5rem" />
+        </button>
+      ) : data ? (
+        <button onClick={handleRemove}>
+          <MdDone size="1.5rem" />
+        </button>
+      ) : (
+        <button onClick={handleRemove}>
+          <MdClose size="1.5rem" />
+        </button>
+      )}
+      <span>{email.value}</span>
+      <style jsx>{`
+        button {
+          background: none;
+          border: none;
+          cursor: pointer;
+          outline: none;
+        }
+        li {
+          align-items: center;
+          display: flex;
+        }
+      `}</style>
+    </li>
+  );
+};
+
+export default forwardRef(EmailListItem);
